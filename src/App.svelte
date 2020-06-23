4c3af097fafcf12baf17d5b36164cf9e953be548
Praneeth <johndoeknown@protonmail.com>
2020-06-23 13:00:59 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+	import AppBar from "./components/AppBar.svelte";
+</script>
+
+<AppBar title={{ label: "Hello World", onClick: () => console.log("Hello World") }} />
---
+++ src/components/AppBar.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let title;
+</script>
+
+<div class="m-4 p-4 rounded-md shadow-md border-2 text-center">
+    <h1 class="text-4xl cursor-pointer" on:click={() => title.onClick()}>{title.label}</h1>
+    <nav class="m-4">
+
+    </nav>
+</div>
