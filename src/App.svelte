b51c631519455cc69a6c50c68211155aa40fbdbf
Praneeth <johndoeknown@protonmail.com>
2020-06-24 09:03:49 +0530

    built OutlineButton and added rounded to RaisedButton

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import AppBar from "./components/AppBar.svelte";
 	import RaisedButton from "./components/RaisedButton.svelte";
+	import OutlineButton from "./components/OutlineButton.svelte";
 </script>
 
 <AppBar title={{ label: "Hello World", onClick: () => console.log("Hello World") }} navLinks={[{label: "Link1", route: "/"}, {label: "Link2", route: "/"}, {label: "Link3", route: "/"}]}/>
@@ -8,3 +9,5 @@
 <RaisedButton label={"Hello World"} onClick={() => console.log("Button Clicked")} />
 
 <RaisedButton label={"Hello World"} onClick={() => console.log("Button Clicked")} darkMode={"true"} />
+
+<OutlineButton label={"Hello World"} onClick={() => console.log("Button Clicked")} />
--- src/components/RaisedButton.svelte
+++ src/components/RaisedButton.svelte
@@ -5,11 +5,11 @@
 </script>
 
 {#if !darkMode}
-    <button class="p-4 m-4 text-xl shadow-md border-2 bg-white active:shadow-none focus:outline-none transition ease-in duration-200" on:click={() => onClick()}>
+    <button class="p-4 m-4 text-xl shadow-md border-2 bg-white active:shadow-none focus:outline-none transition ease-in duration-200 rounded-md" on:click={() => onClick()}>
         {label}
     </button>
 {:else}
-    <button class="p-4 m-4 text-xl text-white active:text-black shadow-md bg-black active:bg-white focus:outline-none transition ease-in duration-200" on:click={() => onClick()}>
+    <button class="p-4 m-4 text-xl text-white active:text-black shadow-md bg-black active:bg-white focus:outline-none transition ease-in duration-200 rounded-md" on:click={() => onClick()}>
         {label}
     </button>
 {/if}
