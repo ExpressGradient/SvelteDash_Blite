adcbf9221ee3c4dcaa01c5eb8ffa0628de9ec56e
Praneeth <johndoeknown@protonmail.com>
2020-06-26 10:07:17 +0530

    added numbering to Code

--- src/components/Code.svelte
+++ src/components/Code.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="m-4 p-4 rounded-md text-md text-green-400 bg-black font-mono md:w-2/3">
-    {#each codes as code}
-        <h3>{code}</h3>
+    {#each codes as code, index}
+        <h3>{index + 1}. {code}</h3>
     {/each}
 </div>
