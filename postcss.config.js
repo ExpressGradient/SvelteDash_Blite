0c1e20ec377b6e4072ecb7ab680ebc0945dc3391
Hariharan <jhariharan2004@gmail.com>
2020-06-27 21:45:11 +0530

    Added CSS minification and autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
-    plugins: [tailwindcss("./tailwind.config.js")]
+    plugins: [
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer,
+    ]
 }
